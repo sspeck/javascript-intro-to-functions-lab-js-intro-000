02cb739afd0ebec0a7164f3ebf5d5919c067ad6c
Sarah Speck <smspeck13@gmail.com>
2018-03-07 03:12:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper (HELLO) {
   console.log('HELLO'.toLowerCase())
 }
 
-function sayHiToGrandma(hello){
-  if(hello.toLowerCase()==='hello'){
+function sayHiToGrandma(string){
+  if(string.toLowerCase()===string){
     return "I can't hear you!"
-  }else if(hello.toUpperCase()==='HELLO'){
+  }else if(string.toUpperCase()===string){
     return "YES INDEED!"
-  }else if (hello === "I love you, Grandma.") {
+  }else if (string === "I love you, Grandma.") {
   return "I love you, too."
 }
 }
